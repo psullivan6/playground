9dd7024882cdb52a36bcf77d2269e937d8888bda
Patrick Sullivan <psullivan6@gmail.com>
2016-04-01 14:44:58 -0700

    superclean task

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -5,3 +5,7 @@ var gulp   = require('gulp');
 gulp.task('clean', function (callback) {
   return del(config.paths.clean, callback);
 });
+
+gulp.task('superclean', function (callback) {
+  return del(config.paths.superclean, callback);
+});
